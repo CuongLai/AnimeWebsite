2daf6e8121fc824bee457c6482d4b4827eac18f9
Cuong Lai <Laic5000@gmail.com>
2017-03-01 23:50:29 -0500

    Added search function to watched.php

--- watched.php
+++ watched.php
@@ -16,49 +16,81 @@ include 'includeFiles/script.php';
 $query = "SELECT pmkAnimeId, fldAnimeName, fldKissLink, fldCrunchyLink, fldMalLink FROM tblAnime";
 $result = $conn->query($query);
 
-
-
 print '<div class="content">';
 
 print '<div class="top"><p class="title">Completed anime</p></div>';
-
-print '<form method="post">';
-print '<div class="btnDiv">';
-print '<input type="submit" name="submit" value="Add an anime" class="button"></div>';
-print '</form>';
-
+?>
+<form method="post">
+<div class="btnDiv">
+<input type="text" class="txtSearch" name="txtSearch" placeholder="Search watched anime"><input type="submit" name="search" value="Search" class="searchBtn">
+<input type="submit" name="submit" value="Add an anime" class="button"></div>
+</form>
+<?php
 if (isset($_POST[submit]))
 {
   header("Location:addAnime.php");
   exit;
 }
+
 print '<table>';
 print '<tr><th>Name</th><th>KissAnime</th><th>Crunchyroll</th><th>MyAnimeList</th></tr>';
-if ($result->num_rows > 0)
+if (isset($_POST[search]))
 {
-  while($row = $result->fetch_assoc())
+  $searchQuery = htmlentities($_POST["txtSearch"], ENT_QUOTES, "UTF-8");
+  $sql = "SELECT * FROM tblAnime WHERE fldAnimeName LIKE '%{$searchQuery}%'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0)
   {
-    print '<tr>';
-    print '<td>' . $row["fldAnimeName"] . '</td>';
-    print '<td>';
-    if (!empty($row['fldKissLink']))
+    while($row = $result->fetch_assoc())
     {
-      print '<a href="'.$row['fldKissLink'].'">';
-      print '<img src="images/kissanime.png" alt="kissIcon"></a>';
+      print '<tr>';
+      print '<td class="nameRow">' . $row["fldAnimeName"] . '</td>';
+      print '<td>';
+      if (!empty($row['fldKissLink']))
+      {
+        print '<a href="'.$row['fldKissLink'].'">';
+        print '<img src="images/kissanime.png" alt="kissIcon"></a>';
+      }
+      print '</td><td>';
+      if (!empty($row['fldCrunchyLink']))
+      {
+        print '<a href="'.$row['fldCrunchyLink'].'">';
+        print '<img src="images/crunchyroll.png" alt="crunchyIcon"></a>';
+      }
+      print '</td><td>';
+      print '<a href="'.$row['fldMalLink'].'"><img src="images/mal-icon.png" alt="MalIcon"></a>';
+      print '</td></tr>';
     }
-    print '</td><td>';
-    if (!empty($row['fldCrunchyLink']))
-    {
-      print '<a href="'.$row['fldCrunchyLink'].'">';
-      print '<img src="images/crunchyroll.png" alt="crunchyIcon"></a>';
-    }
-    print '</td><td>';
-    print '<a href="'.$row['fldMalLink'].'"><img src="images/mal-icon.png" alt="MalIcon"></a>';
-    print '</td></tr>';
   }
-  print '   </table>';
-  print '</div>';
 }
+else
+{
+  if ($result->num_rows > 0)
+  {
+    while($row = $result->fetch_assoc())
+    {
+      print '<tr>';
+      print '<td class="nameRow">' . $row["fldAnimeName"] . '</td>';
+      print '<td>';
+      if (!empty($row['fldKissLink']))
+      {
+        print '<a href="'.$row['fldKissLink'].'">';
+        print '<img src="images/kissanime.png" alt="kissIcon"></a>';
+      }
+      print '</td><td>';
+      if (!empty($row['fldCrunchyLink']))
+      {
+        print '<a href="'.$row['fldCrunchyLink'].'">';
+        print '<img src="images/crunchyroll.png" alt="crunchyIcon"></a>';
+      }
+      print '</td><td>';
+      print '<a href="'.$row['fldMalLink'].'"><img src="images/mal-icon.png" alt="MalIcon"></a>';
+      print '</td></tr>';
+    }
+  }
+}
+print '   </table>';
+print '</div>';
 
 print '</body>';
 include 'includeFiles/footer.php'
