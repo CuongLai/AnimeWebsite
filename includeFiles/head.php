f2c3e292c1f59a33fccd81ca0d37a1595ebc2628
CuongLai <Laic5000@gmail.com>
2017-06-26 12:21:24 -0400

    Fixed mobile issue

--- includeFiles/head.php
+++ includeFiles/head.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
   <title>Personal Anime Site</title>
-  <meta charset="utf-8">
-  <meta name="author" content="Cuong Lai">
-  <meta name="description" content="My personal stuff">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <link rel="stylesheet"
   	  href="style.css"
